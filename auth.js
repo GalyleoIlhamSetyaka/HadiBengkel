1864b3f5cc9129d86c00bc86a77f591497b00a39
GalyleoIlhamSetyaka <ilhamsetyaka14@gmail.com>
2024-09-10 14:30:14 +0700

    login function

--- auth.js
+++ auth.js
@@ -1,11 +1,12 @@
  // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
- import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+ import {getAuth,onAuthStateChanged,signOut, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
  import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
  
  const firebaseConfig = {
     apiKey: "AIzaSyBRbKqEeEFTU82OtGXOwsFIxX8QnkPsuJ8",
     authDomain: "hadibengkel-8abda.firebaseapp.com",
+    databaseURL: "https://hadibengkel-8abda-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "hadibengkel-8abda",
     storageBucket: "hadibengkel-8abda.appspot.com",
     messagingSenderId: "154359113405",
@@ -15,18 +16,12 @@
 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
-
- function showMessage(message, divId){
-    var messageDiv=document.getElementById(divId);
-    messageDiv.style.display="block";
-    messageDiv.innerHTML=message;
-    messageDiv.style.opacity=1;
-    setTimeout(function(){
-        messageDiv.style.opacity=0;
-    },5000);
- }
+ const auth = getAuth();
  const signIn=document.getElementById('submitSignIn');
- signIn.addEventListener('click', (event)=>{
+ const logoutButton=document.getElementById('logout');
+
+
+ signIn?.addEventListener('click', (event)=>{
     event.preventDefault();
     const email=document.getElementById('email').value;
     const password=document.getElementById('password').value;
@@ -37,7 +32,7 @@
         showMessage('login is successful', 'signInMessage');
         const user=userCredential.user;
         localStorage.setItem('loggedInUserId', user.uid);
-        window.location.href='index.html';
+        window.location.href='admin.html';
     })
     .catch((error)=>{
         const errorCode=error.code;
@@ -49,3 +44,35 @@
         }
     })
  })
+
+ function showMessage(message, divId){
+    var messageDiv=document.getElementById(divId);
+    messageDiv.style.display="block";
+    messageDiv.innerHTML=message;
+    messageDiv.style.opacity=1;
+    setTimeout(function(){
+        messageDiv.style.opacity=0;
+    },5000);
+ }
+
+ logoutButton.addEventListener('click',()=>{
+   localStorage.removeItem('loggedInUserId');
+   signOut(auth)
+   .then(()=>{
+       window.location.href='index.html';
+   })
+   .catch((error)=>{
+       console.error('Error Signing out:', error);
+   })
+ })
+
+ auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log(user.email + " is logged in!");
+    } else {
+      console.log('User is logged out!');
+      if (window.location.pathname.includes("admin.html")) {
+        window.location.href = "login.html";
+    }
+    }
+  });
